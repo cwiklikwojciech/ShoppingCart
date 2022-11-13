502b64e18df8c0e9426d6916f242199a30714c1b
wojciech.cwiklik <cwiklik.wojciech@gmail.com>
2022-11-13 17:47:02 +0100

    [main] - Delete tailwind, modal with promotion.

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,25 +1,32 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import {Col, Row} from 'react-bootstrap'
 import "./Modal.css";
+
+import marketingImage from "/imgs/project.jpg";
 
 export function Example (props) {
     console.log(props);
+   
     return (
         <Modal
           {...props}
-          size="lg"
+          // size="lg"
           aria-labelledby="contained-modal-title-vcenter"
           centered
         >
         
-          <Modal.Header closeButton />
+          
           <Modal.Body>
-            Test
+            <Row>
+              <Col lg={7} md={7} sm={7} xs={7} className="modal-newsletter-container">
+                
+              </Col>
+              <Col lg={5} md={5} sm={5} xs={5} style={{backgroundImage: `url(${marketingImage})`, backgroundSize: "cover", position: "relative"}}>
+                <div className="body-container"></div>
+              </Col>
+            </Row>
           </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={props.onHide}>Close</Button>
-          </Modal.Footer>
-         
         </Modal>
       );
 }
